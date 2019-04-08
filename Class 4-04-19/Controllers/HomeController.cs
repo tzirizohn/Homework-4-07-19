826ab12ec45260087aaa931303dd88be1667dc3c
Tziri <tzirizohn1@gmail.com>
2019-04-07 23:57:32 -0400

    Homework 4-04-19

--- Class 4-04-19/Controllers/HomeController.cs
+++ Class 4-04-19/Controllers/HomeController.cs
@@ -2,29 +2,116 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.Mvc;
+using Class_4_04_19.Models;
+using System.Web.Security;
 
 namespace Class_4_04_19.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
+        public ActionResult Load()
+        {
+            PassWordManager pm = new PassWordManager(Properties.Settings.Default.Const);
+            ViewModel2 vm = new ViewModel2();
+            vm.IsAuthenticated = User.Identity.IsAuthenticated;
+            if (vm.IsAuthenticated)
+            {
+                Users u = pm.GetEmail(User.Identity.Name);
+                vm.id = u.id;
+            }
+
+            return View(vm);
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult AddUser(Users user)
         {
-            ViewBag.Message = "Your application description page.";
+            PassWordManager pm = new PassWordManager(Properties.Settings.Default.Const);
+            pm.AddUser(user);
+            return Redirect("/home/login");
+        }
 
+        public ActionResult Login()
+        {
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult LoginToAccount(string email, string password)
         {
-            ViewBag.Message = "Your contact page.";
+            PassWordManager pm = new PassWordManager(Properties.Settings.Default.Const);
+            Users user = pm.Login(email, password);
+            if (user == null)
+            {
+                return Redirect("/home/login");
+            }
 
-            return View();
+            FormsAuthentication.SetAuthCookie(email, true);
+            return Redirect("/home/load");
+        }
+
+        public ActionResult Upload(HttpPostedFileBase image, Images images, string password, int userid)
+        {
+            string ext = Path.GetExtension(image.FileName);
+            string filename = $"{Guid.NewGuid()}{ext}";
+            string fullpath = $"{Server.MapPath("/UploadedImages")}\\{filename}";
+            image.SaveAs(fullpath);
+            PassWordManager pm = new PassWordManager(Properties.Settings.Default.Const);
+            images.FileName = filename;
+            int id = pm.AddImage(images, password, userid);
+            images.id = id;
+            return View(images);
+        }
+
+        public ActionResult EnterPassword(int id, string text)
+        {
+            PassWordManager pm = new PassWordManager(Properties.Settings.Default.Const);
+            Images i = pm.GetImage(id, text);
+            ViewModel vm = new ViewModel();
+            vm.Image = i;
+
+            if (Session["password"] == null)
+            {
+                Session["password"] = new List<int>();
+            }
+
+            List<int> ids = (List<int>)Session["password"];
+            if (ids.Contains(id))
+            {
+                text = i.Password;
+            }
+            else if (text == i.Password)
+            {
+                ids.Add(id);
+            }
+
+            if (text != i.Password)
+            {
+                vm.Password = text;
+                vm.IncorrectPassword = true;
+            }
+            else
+            {
+                vm.Password = text;
+                vm.IncorrectPassword = false;
+                int x = pm.Count(id);
+                int y = pm.AddToCount(x, id);
+                vm.Image.Count = y;
+            }
+            return View(vm);
+        }
+
+        public ActionResult MyAccount(int userid)
+        {
+            PassWordManager pm = new PassWordManager(Properties.Settings.Default.Const);
+            IEnumerable<Images>img= pm.MyAccount(userid);
+            return View(img);
         }
     }
 }
---
+++ Class 4-04-19/Models/Images.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web; 
+
+namespace Class_4_04_19.Models
+{
+    public class Images
+    {
+        public string Password { get; set; }
+        public string FileName { get; set; }
+        public int id { get; set; }
+        public string text { get; set; }
+        public int Count;                                 
+    }
+
+    public class ViewModel
+    {
+        public Images Image { get; set; }
+        public bool IncorrectPassword { get; set; }
+        public string Password { get; set; }
+    }   
+
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int id { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class ViewModel2
+    {
+        public int id { get; set; }         
+        public bool IsAuthenticated { get; set; }
+    }
+
+    public class PassWordManager
+    {
+        private string _connectionString;
+
+        public PassWordManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int AddImage(Images i, string password, int userid)
+        {                     
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "insert into images values (@filename,@password,@count,@userid) select SCOPE_IDENTITY()";
+            cmd.Parameters.AddWithValue("@filename", i.FileName);
+            cmd.Parameters.AddWithValue("@password", i.Password);
+            cmd.Parameters.AddWithValue("@count", i.Count);
+            cmd.Parameters.AddWithValue("@userid",userid);
+            conn.Open();
+            return (int)(decimal)cmd.ExecuteScalar();
+        }
+
+        public void AddUser(Users user)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            user.Password = hash;
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "insert into users values(@name, @email, @password)";
+            cmd.Parameters.AddWithValue("@name", user.Name);
+            cmd.Parameters.AddWithValue("@email", user.Email);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        public Users GetEmail(string email)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from users where email=@email";
+            cmd.Parameters.AddWithValue("@email", email);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Users users = new Users();
+            if (!reader.Read())
+            {
+                return null;
+            }
+
+            {
+                users.Name = (string)reader["Name"];
+                users.Email = (string)reader["email"];
+                users.Password = (string)reader["Password"];
+                users.id = (int)reader["id"];  
+            }
+            return users;
+        }
+          
+        public Users Login(string email, string password)
+        {
+            Users user = GetEmail(email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            bool isValid = BCrypt.Net.BCrypt.Verify(password, user.Password);
+            if (!isValid)
+            {
+                return null;
+            }
+
+            return user;
+        }         
+
+        public Images GetImage(int id, string hi)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from images where id=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Images images = new Images();
+            while (reader.Read())
+            {
+                images.FileName = (string)reader["FileName"];
+                images.Password = (string)reader["Password"];
+                images.id = (int)reader["id"];
+                images.text = hi;
+                images.Count = (int)reader["count"];
+            }
+            return images;
+        }
+
+        public int Count(int id)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select count from images where id= @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            conn.Open();
+            int count = (int)cmd.ExecuteScalar();
+            return count;
+        }
+
+        public int AddToCount(int count, int id)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            count++;
+            cmd.CommandText = "update images set count=@count where id=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@count", count);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            return count;
+        }
+
+        public IEnumerable<Images> MyAccount(int userid)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from images where userid= @id";
+            cmd.Parameters.AddWithValue("@id", userid);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Images>images = new List<Images>();
+            while (reader.Read())
+            {   
+                images.Add(new Images
+                {
+                    FileName = (string)reader["FileName"],
+                    Password = (string)reader["Password"],
+                    Count = (int)reader["count"]
+                });   
+            }
+            return images;
+        }
+    }
+}
